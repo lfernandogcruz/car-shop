e2d8deac672b0ee0557d6895d0fc390bb0a8baa0
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 19:22:05 -0300

    feat: req 04 - implements route /cars on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', routes.carsRouter);
 
 export default app;
 // new line for first commit
