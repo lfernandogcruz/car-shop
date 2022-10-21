af4dd850845b05fb6b488299aac0d60653d0e538
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 17:07:13 -0300

    refact: refactor IVehicle interface using Zod

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,29 @@
-export interface IVehicle {
-  model: string, // Deve ser uma string com, pelo menos, 3 caracteres
-  year: number, // Deve ser um valor inteiro positivo >= 1900, porém <= 2022
-  color: string, // Deve ser uma string com, pelo menos, 3 caracteres
-  status?: boolean, // Deve receber valores booleanos e deve ser opcional
-  buyValue: number, // Deve receber apenas números inteiros
-}
+import { z } from 'zod';
+
+export const vehicleZodSchema = z.object({
+  model: z.string({
+    required_error: 'Model is required',
+  }).min(3, {
+    message: 'Model must be at least 3 characters',
+  }),
+  year: z.number({
+    required_error: 'Year is required',
+  }).int().min(1900, {
+    message: 'Year must be at least 1900',
+  }).max(2022, {
+    message: 'Year must be at most 2022',
+  }),
+  color: z.string({
+    required_error: 'Color is required',
+  }).min(3, {
+    message: 'Color must be at least 3 characters',
+  }),
+  status: z.boolean().optional(),
+  buyValue: z.number({
+    required_error: 'Buy value is required',
+  }).int().min(0, {
+    message: 'Buy value must be at least 0',
+  }),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
