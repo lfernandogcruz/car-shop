75129194fb53bbf04c4c14f59bb71c46653a2e58
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 22:34:53 -0300

    feat: implements error middleware on app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import routes from './routes/index';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/cars', routes.carsRouter);
+
+app.use(errorHandler);
 
 export default app;
 // new line for first commit
