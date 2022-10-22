5681fe5337ddcd1d098d165c085100d9c7f6c465
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 22:37:35 -0300

    feat: creates MongoModel for connection with the DB

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,36 @@
+import { isValidObjectId, Model } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model:Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj:T):Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read():Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async readOne(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOne({ _id });
+  }
+
+  public async update(_id:string, obj:Partial<T>):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOneAndUpdate({ _id }, { ...obj }, { new: true });
+  }
+
+  public async delete(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
+    return this._model.findOneAndDelete({ _id });
+  }
+}
+
+export default MongoModel;
