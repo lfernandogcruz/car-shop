ec546bf0beb6176bfdadc2766e41e67f72a3e0fa
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 22:33:38 -0300

    feat: errors catalog based on the one shown at the course platform

---
+++ src/errors/catalog.ts
@@ -0,0 +1,29 @@
+export enum ErrorTypes {
+  EntityNotFound = 'EntityNotFound',
+  InvalidMongoId = 'InvalidMongoId',
+  InvalidCarId = 'InvalidCarId',
+}
+
+type ErrorResponseObject = {
+  message: string;
+  httpStatus: number;
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject;
+};
+
+export const errorCatalog: ErrorCatalog = {
+  EntityNotFound: {
+    message: 'Entity not found',
+    httpStatus: 404,
+  },
+  InvalidMongoId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    httpStatus: 400,
+  },
+  InvalidCarId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    httpStatus: 400,
+  },
+};
