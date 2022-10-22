1ca7af13f69c4c79b740b34ba89ec0b878f3e85d
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-21 22:31:30 -0300

    refact: adds Partial to allow updates with incomplete obj description

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -2,6 +2,7 @@ export interface IModel <T>{
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
   readOne(str: string): Promise<T | null>,
-  update(str: string, obj: T): Promise<T | null>,
+  update(str: string, obj: Partial<T>): Promise<T | null>,
+  // Partial<T> is a type that allows us to update only some of the properties of an object
   delete(str: string): Promise<T | null>,
 }
